0d0c8ca17019bc5f3f9056fd39a90414b4f2e4cf
VPKillerQueen <VPKillerQueen@users.noreply.github.com>
2024-03-14 12:53:30 +0700

    failed adding jwt authentication so back to the previous point

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,21 +3,18 @@
 import Image from "next/image";
 import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthContext from "./AuthProvider";
+
 const Header = () => {
-  let { user, logoutUser } = useContext<any>(AuthContext);
   return (
     <div className="flex justify-between p-2  shadow-lg shadow-blue-500/50 items-center">
       <div>
-        <Link to="/">
-          <Image
-            width={60}
-            height={60}
-            className="h-auto"
-            src="/images/logoBK.png"
-            alt="logo"
-          />
-        </Link>
+        <Image
+          width={60}
+          height={60}
+          className="h-auto"
+          src="/images/logoBK.png"
+          alt="logo"
+        />
       </div>
       <div className="flex gap-2 items-center">
         <div className="rounded-full overflow-hidden w-[60px] h-[60px] border-gray-300 border-[1px] flex items-center justify-center">
@@ -30,12 +27,6 @@ const Header = () => {
           />
         </div>
         <h3>Tri Dung</h3>
-        {user ? (
-          <p onClick={logoutUser}>Logout</p>
-        ) : (
-          <Link to="/login">Login</Link>
-        )}
-        {user && <p>Hello {user.username}!</p>}
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,34 +3,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import * as React from "react";
-import Login from "./login/page";
 import MainLayout from "./components/MainLayout";
 import { ClassNames } from "@emotion/react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import PrivateRoute from "./utils/PrivateRoutes";
-import { AuthProvider } from "./components/AuthProvider";
+// import PrivateRoute from "./utils/PrivateRoutes";
+// import { AuthProvider } from "./components/test";
 
 export default function Home() {
-  const isAuthenticated = true;
   return (
     <div className="Web">
-      <Router>
-        <AuthProvider>
-          <Header />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <PrivateRoute>
-                  <MainLayout>Homepage</MainLayout>
-                </PrivateRoute>
-              }
-            />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </AuthProvider>
-      </Router>
+      <MainLayout>HOME</MainLayout>
     </div>
   );
 }
