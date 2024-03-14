ada74f605c22ca7c0187badf1bfd34bcd429fe41
VPKillerQueen <VPKillerQueen@users.noreply.github.com>
2024-03-14 13:15:50 +0700

    trying to publish 2

---
+++ app/details/type1/type1file.tsx
@@ -0,0 +1,18 @@
+"use client";
+import MainLayout from "@/app/components/MainLayout";
+import MainView from "@/app/components/MainView";
+
+const Type1 = () => {
+  return (
+    <>
+      <MainLayout>
+        <MainView />
+        {/* <div className="App">
+          <MyChart chartData={chartdata} /> */}
+        {/* </div> */}
+      </MainLayout>
+    </>
+  );
+};
+
+export default Type1;
